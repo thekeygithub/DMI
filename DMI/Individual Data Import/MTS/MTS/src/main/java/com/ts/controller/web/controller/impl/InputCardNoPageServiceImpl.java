931c8008c65c3ee94848874cc3633b58e44ed484
thekeygithub <33211781+thekeygithub@users.noreply.github.com>
2018-03-30 11:10:14 +0800

    提交web 一部分功能

---
+++ DMI/Individual Data Import/MTS/MTS/src/main/java/com/ts/controller/web/controller/impl/InputCardNoPageServiceImpl.java
@@ -0,0 +1,47 @@
+package com.models.cloud.web.controller.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import com.models.cloud.util.hint.Hint;
+import com.models.cloud.web.controller.DoWebPageService;
+/**
+ * 输入银行卡号
+ * @Description
+ * @encoding UTF-8 
+ * @author haiyan.zhang
+ * @date 2016年5月19日 
+ * @time 下午3:11:38
+ * @version V1.0
+ * @修改记录
+ *
+ */
+@Service("pcInputCardNoPageServiceImpl")
+public class InputCardNoPageServiceImpl implements DoWebPageService {
+	private Logger logger = Logger.getLogger(InputCardNoPageServiceImpl.class);
+	
+	@Override
+	public Map<String, Object> returnData(Map<String, Object> map,Model model, HttpServletRequest request) {
+		if(logger.isInfoEnabled()){
+			logger.info("进入输入银行卡号页面");
+		}
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("resultCode", Hint.SYS_SUCCESS.getCodeString());
+		resultMap.put("resultDesc", Hint.SYS_SUCCESS.getMessage());
+		
+		return resultMap;
+	}
+
+	@Override
+	public Map<String, Object> returnDataByMap(Map<String, Object> map, HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
