a7a98647b5e6ccb8f7a8c3d243c7d636546c6255
thekeygithub <33211781+thekeygithub@users.noreply.github.com>
2018-04-20 15:31:47 +0800

    提交

---
+++ DMI/Business Intelligence/BIPlatform/src/com/ts/h5/controller/impl/InputPWDUnBindPageServiceImpl.java
@@ -0,0 +1,43 @@
+package com.models.cloud.h5.controller.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import com.models.cloud.h5.controller.DoPageService;
+import com.models.cloud.util.hint.Hint;
+
+@Service("inputPWDUnBindPageServiceImpl")
+public class InputPWDUnBindPageServiceImpl implements DoPageService {
+	private Logger logger = Logger.getLogger(InputPWDUnBindPageServiceImpl.class);
+	
+	@Override
+	public Map<String, Object> returnData(Map<String, Object> map,Model model, HttpServletRequest request) {
+		if(logger.isInfoEnabled()){
+			logger.info("跳转到输入密码验证身份页面");
+		}
+		
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		String bindId = request.getParameter("bindId");
+		if(logger.isInfoEnabled()){
+			logger.info("跳转到输入密码验证身份页面，bindId："+bindId);
+		}
+		resultMap.put("bindId", bindId);
+		resultMap.put("resultCode", Hint.SYS_SUCCESS.getCodeString());
+		resultMap.put("resultDesc", Hint.SYS_SUCCESS.getMessage());
+		
+		return resultMap;
+	}
+
+	@Override
+	public Map<String, Object> returnDataByMap(Map<String, Object> map, HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ DMI/Business Intelligence/BIPlatform/src/com/ts/h5/controller/impl/ToResultFailPageServiceImpl.java
@@ -0,0 +1,38 @@
+package com.models.cloud.h5.controller.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import com.models.cloud.h5.controller.DoPageService;
+import com.models.cloud.util.hint.Hint;
+
+@Service("toResultFailPageServiceImpl")
+public class ToResultFailPageServiceImpl implements DoPageService {
+	private Logger logger = Logger.getLogger(ToResultFailPageServiceImpl.class);
+	
+	@Override
+	public Map<String, Object> returnData(Map<String, Object> map, Model model, HttpServletRequest request) {
+		 Map<String, Object> resultMap = new HashMap<String, Object>();
+		 resultMap.put("failDescription", request.getParameter("failDescription"));
+		 if(logger.isInfoEnabled()){
+			 logger.info("跳转到支付失败结果页面，传入参数：" + resultMap.toString());
+		 }
+	        
+		 resultMap.put("resultCode", Hint.SYS_SUCCESS.getCodeString());
+		 resultMap.put("resultDesc", Hint.SYS_SUCCESS.getMessage());
+		 return resultMap;
+	}
+
+	@Override
+	public Map<String, Object> returnDataByMap(Map<String, Object> map, HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
